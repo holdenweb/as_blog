c80a1f687eb21326c405c75a43bf679512253f31
Steve Holden <steve@holdenweb.com>
2020-11-05 15:47:04 +0000

    Add textStyle parsing and refactor debug outputs for easier control.

--- src/tools/parse_blog.py
+++ src/tools/parse_blog.py
@@ -18,12 +18,11 @@ SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
 # The ID of a sample document.
 DOCUMENT_ID = '1jALRWW76qjrcl12e-umDm-ZbGlm8HcGuaA2jGdl1Zro'
 
-rprint = print
+DEBUG_FEATURE_FLAGS = {"tr", "ts", "ps"}
 
-def print(*args, **kw):
-    return
-    return rprint(*args, **kw)
-
+def debug(debug_class, *arg, **kw):
+    if debug_class in DEBUG_FEATURE_FLAGS:
+        print(*arg, **kw)
 
 class MyDoc():
     """
@@ -40,14 +39,14 @@ class MyDoc():
         Parses the given document element by recursively parsing
         the content of each item.
         """
-        print(f"Parsing", ".".join(ancestors+[element_name]))
+        debug("parse", f"Parsing", ".".join(ancestors+[element_name]))
         for item_name in item_names:
             if item_name in element:
                 method = getattr(self, f"parse_{item_name}", self.parse_unrecognised_part_name)
                 item = element[item_name]
-                print("Handling", ".".join(ancestors+[element_name, item_name]))
+                debug("parse", "Handling", ".".join(ancestors+[element_name, item_name]))
                 method(element=item, element_name=item_name, ancestors=ancestors+[element_name])
-        print(f"Parse of", ".".join(ancestors+[element_name]), 'complete')
+        debug("parse", f"Parse of", ".".join(ancestors+[element_name]), 'complete')
 
     def parse_body(self, element, element_name, ancestors):
         item_names = ('content', )
@@ -58,7 +57,7 @@ class MyDoc():
         elements is a sequence of structuralElement objects, some of which should
         be parsed to access publishable content.
         """
-        print("Content elements count:", len(element))
+        debug("content", "Content elements count:", len(element))
         for item in element:
             self.parse_structuralElement(item, 'structuralElement', ancestors=ancestors)
 
@@ -76,7 +75,7 @@ class MyDoc():
 
     def parse_element(self, element, element_name, ancestors):
         item_names = ('textRun', 'autoText', 'pageBreak', 'columnBreak', 'footnoteReference', 'horizontalRule', 'equation', 'inlineOPbnjectElement')
-        print(f"Range: {element['startIndex']}-{element['endIndex']}")
+        debug("element", f"Range: {element['startIndex']}-{element['endIndex']}")
         self.parse(element, element_name, item_names, ancestors)
 
     def parse_paragraph(self, element, element_name, ancestors):
@@ -85,31 +84,40 @@ class MyDoc():
 
     def parse_paragraphStyle(self, element, element_name, ancestors):
         self.p_styles.push(element)
-        rprint(f"Pushed {element_name} {element!r}")
-        rprint("Style now:")
+        debug("ps", f"Pushed {element_name} {element!r}")
+        debug("ps", "Style now:")
         for key, value in sorted(self.p_styles.to_dict().items()):
-            print(f"{key!r}: {value!r}")
+            if value:
+                debug("ps", f":::{key!r}: {value!r}")
         assert self.p_styles.pop() == element
 
+    def parse_textStyle(self, element, element_name, ancestors):
+        self.t_styles.push(element)
+        debug("ts", f"Pushed {element_name} {element!r}")
+        debug("ts", "Style now:")
+        for key, value in sorted(self.p_styles.to_dict().items()):
+            debug("ts", f"...{key!r}: {value!r}")
+        assert self.t_styles.pop() == element
+
     def parse_sectionBreak(self, element, element_name, ancestors):
-        print("sectionBreak:", element)
+        debug("sb", "sectionBreak:", element)
 
     def parse_structuralElement(self, element, element_name, ancestors):
         part_names = ('sectionBreak', 'tableOfContents', 'table', 'paragraph')
         return self.parse(element, element_name='structuralElement', item_names=part_names, ancestors=ancestors)
 
     def parse_textRun(self, element, element_name, ancestors):
-        print(element['content'], end="|")
-        self.content.append((element.get('textStyle', "NO STYLE"), element['content']))
-        style = element.get('textStyle', "NO STYLE")
-        print("Style:", style if style else "EMPTY STYLE")
+        style = element.get('textStyle', "")
+        self.content.append((style, element['content']))
+        debug("tr", "tStyle:", style if style else "UNSTYLED CONTENT")
+        debug("tr", element['content'], end="|")
 
     def parse_title(self, element, element_name, ancestors):
         self.title = element
-        print(".".join(ancestors+[element_name]), "is", element)
+        debug("t", ".".join(ancestors+[element_name]), "is", element)
 
     def parse_unrecognised_part_name(self, element, element_name, ancestors):
-        print("Didn't handle", '.'.join(ancestors+[element_name]))
+        debug("upn", "Didn't handle", '.'.join(ancestors+[element_name]))
 
 
 def main():
@@ -145,9 +153,9 @@ def main():
     parser = MyDoc()
     parser.parse_document(element=document, ancestors=[])
 
-    print(f'The title of document {DOCUMENT_ID} is: {document.get("title")!r}')
-    print(f'  The parser says:', parser.title)
-    print(f'                  ', parser.documentId)
+    debug("main", f'The title of document {DOCUMENT_ID} is: {document.get("title")!r}')
+    debug("main", f'  The parser says:', parser.title)
+    debug("main", f'                  ', parser.documentId)
 
     from itertools import groupby
     current_style = None
@@ -155,8 +163,8 @@ def main():
     for style, para in ci:
         if style != current_style:
             current_style = style
-            print(f"\n:::::::: Style: {style} ::::::::")
-        print(para, sep="", end='')
+            debug(f"\n:::::::: Style: {style} ::::::::")
+        debug(para, sep="", end='')
 
 
 
