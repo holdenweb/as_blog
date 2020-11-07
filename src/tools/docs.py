ae7b7a259605fe39ced03bb6deb6ce6617e4443d
Steve Holden <steve@holdenweb.com>
2020-11-07 22:20:31 +0000

    Separate out file handling. Start parsing document styles.

---
+++ src/tools/docs.py
@@ -0,0 +1,68 @@
+import json
+import os
+import pickle
+import sys
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ["https://www.googleapis.com/auth/documents.readonly"]
+
+DEBUG_FEATURE_FLAGS = {"final"}
+CACHE_DIRECTORY = os.path.expanduser("~/.docs_cache")
+
+
+def debug(debug_class, *arg, **kw):
+    if debug_class in DEBUG_FEATURE_FLAGS or "all" in DEBUG_FEATURE_FLAGS:
+        print(*arg, **kw)
+
+
+class DocsFile:
+    def __init__(self, document_id: str):
+        self.document_id = document_id
+        self.cached_path = f"{CACHE_DIRECTORY}/{document_id}.json"
+
+    def cached(self):
+        return os.path.exists(self.cached_path)
+
+    def open(self):
+        if not self.cached():  # Could check mod date and commit new version?
+            self.retrieve(self.document_id)
+        return open(self.cached_path)
+
+    def retrieve(self):
+        """
+        Shows basic usage of the Docs API.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.pickle"):
+            with open("token.pickle", "rb") as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.pickle", "wb") as token:
+                pickle.dump(creds, token)
+
+        service = build("docs", "v1", credentials=creds)
+
+        # Retrieve the documents contents from the Docs service.
+        document = service.documents().get(documentId=self.document_id).execute()
+        with open(self.cached_path, "w") as fp:
+            json.dump(document, fp)
+
+
+if __name__ == "__main__":
+    DocsFile(document_id=sys.argv[1]).retrieve()
--- src/tools/parse_blog.py
+++ src/tools/parse_blog.py
@@ -1,56 +1,63 @@
 import json
-import pickle
-import os.path
+import sys
 
+from docs import DocsFile
+from hu import ObjectDict
 from styles import StyleStack
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
 
 #
 # Next steps: add paragraphStyle handler to action formatting
 # changes, and paragraphElement.footnoteReference handler to
 # ensure that the footnotes are included.
 #
-# The ID of a sample document.
-DOCUMENT_ID = '1jALRWW76qjrcl12e-umDm-ZbGlm8HcGuaA2jGdl1Zro'
 
-DEBUG_FEATURE_FLAGS = {"tr", "ts", "ps"}
+
+DEBUG_FEATURE_FLAGS = {"parse", "upn"}
+
 
 def debug(debug_class, *arg, **kw):
-    if debug_class in DEBUG_FEATURE_FLAGS:
+    if any(flag in DEBUG_FEATURE_FLAGS for flag in (debug_class, "all")):
         print(*arg, **kw)
 
-class MyDoc():
+
+class MyDoc:
     """
     This is a general framework to handle Google documents, allowing their
     processing into blog entries in a relatively automated way.
     """
+
     def __init__(self):
         self.content = []
-        self.p_styles = StyleStack('paragraph')
-        self.t_styles = StyleStack('text')
+        self.p_styles = StyleStack("paragraph")
+        self.t_styles = StyleStack("text")
 
     def parse(self, element, element_name, item_names, ancestors):
         """
         Parses the given document element by recursively parsing
         the content of each item.
         """
-        debug("parse", f"Parsing", ".".join(ancestors+[element_name]))
+        debug("parse", f"Parsing {'.'.join(ancestors + [element_name])}")
         for item_name in item_names:
             if item_name in element:
-                method = getattr(self, f"parse_{item_name}", self.parse_unrecognised_part_name)
+                method = getattr(
+                    self, f"parse_{item_name}", self.parse_unrecognised_part_name
+                )
                 item = element[item_name]
-                debug("parse", "Handling", ".".join(ancestors+[element_name, item_name]))
-                method(element=item, element_name=item_name, ancestors=ancestors+[element_name])
-        debug("parse", f"Parse of", ".".join(ancestors+[element_name]), 'complete')
+                debug(
+                    "parse", "Handling", ".".join(ancestors + [element_name, item_name])
+                )
+                method(
+                    element=item,
+                    element_name=item_name,
+                    ancestors=ancestors + [element_name],
+                )
+        debug("parse", f"Parse of {'.'.join(ancestors + [element_name])} complete")
 
     def parse_body(self, element, element_name, ancestors):
-        item_names = ('content', )
-        return self.parse(element, element_name='content', item_names=item_names, ancestors=ancestors)
+        item_names = ("content",)
+        return self.parse(
+            element, element_name="content", item_names=item_names, ancestors=ancestors
+        )
 
     def parse_content(self, element, element_name, ancestors):
         """
@@ -59,114 +66,157 @@ class MyDoc():
         """
         debug("content", "Content elements count:", len(element))
         for item in element:
-            self.parse_structuralElement(item, 'structuralElement', ancestors=ancestors)
+            self.parse_structuralElement(item, "structuralElement", ancestors=ancestors)
 
     def parse_document(self, element, ancestors=[]):
-        item_names = ('title', 'body', 'footnotes', 'documentStyle', 'namedStyles',
-                      'revisionId', 'suggestionsViewMode', 'documentId')
-        return self.parse(element=element, element_name="document", item_names=item_names, ancestors=ancestors)
+        item_names = (
+            "title",
+            "body",
+            "footnotes",
+            "documentStyle",
+            "namedStyles",
+            "revisionId",
+            "suggestionsViewMode",
+            "documentId",
+        )
+        return self.parse(
+            element=element,
+            element_name="document",
+            item_names=item_names,
+            ancestors=ancestors,
+        )
 
     def parse_documentId(self, element, element_name, ancestors):
         self.documentId = element
 
+    def parse_documentStyle(self, element, element_name, ancestors):
+        item_names = (
+            "background",
+            "defaultHeaderId",
+            "defaultFooterId",
+            "evenPageHeaderId",
+            "evenPageFooterId",
+            "firstPageHeaderId",
+            "firstPageFooterId",
+            "useFirstPageHeaderFooter",
+            "useEvenPageHeaderFooter",
+            "pageNumberStart",
+            "marginTop",
+            "marginBottom",
+            "marginRight",
+            "marginLeft",
+            "pageSize",
+            "marginHeader",
+            "marginFooter",
+            "useCustomHeaderFooterMargins",
+        )
+        self.parse(element, element_name, item_names, ancestors)
+
     def parse_elements(self, element, element_name, ancestors):
         for item in element:
-            self.parse_element(item, 'paragraphElement', ancestors)
+            self.parse_element(item, "paragraphElement", ancestors)
 
     def parse_element(self, element, element_name, ancestors):
-        item_names = ('textRun', 'autoText', 'pageBreak', 'columnBreak', 'footnoteReference', 'horizontalRule', 'equation', 'inlineOPbnjectElement')
+        item_names = (
+            "textRun",
+            "autoText",
+            "pageBreak",
+            "columnBreak",
+            "footnoteReference",
+            "horizontalRule",
+            "equation",
+            "inlineOPbnjectElement",
+        )
         debug("element", f"Range: {element['startIndex']}-{element['endIndex']}")
         self.parse(element, element_name, item_names, ancestors)
 
     def parse_paragraph(self, element, element_name, ancestors):
-        item_names = ('paragraphStyle', 'bullet', 'elements')
+        item_names = ("paragraphStyle", "bullet", "elements")
         return self.parse(element, element_name, item_names, ancestors)
 
     def parse_paragraphStyle(self, element, element_name, ancestors):
+        """
+        Attempts to establish a paragraph style.
+
+        Unfortunately the style's lifetime should be the
+        lifetime of its containing paragraph, so a simple
+        push and pop are inappropriate.
+
+        Since the style is always processed before the content,
+        it's appropriate to set the style here, but it cannot
+        be popped until the end of the paragraph.
+
+        Of course if the paragraphStyle element is optional,
+        there will also need to be some mechanism that guards
+        the style pop option so it only occurs when the push
+        has been performed. Through a glass darkly I can see
+        the shape of an abstract syntax tree.
+        """
         self.p_styles.push(element)
         debug("ps", f"Pushed {element_name} {element!r}")
-        debug("ps", "Style now:")
+        debug("ps", "pStyle now:")
         for key, value in sorted(self.p_styles.to_dict().items()):
             if value:
                 debug("ps", f":::{key!r}: {value!r}")
         assert self.p_styles.pop() == element
 
-    def parse_textStyle(self, element, element_name, ancestors):
-        self.t_styles.push(element)
-        debug("ts", f"Pushed {element_name} {element!r}")
-        debug("ts", "Style now:")
-        for key, value in sorted(self.p_styles.to_dict().items()):
-            debug("ts", f"...{key!r}: {value!r}")
-        assert self.t_styles.pop() == element
-
     def parse_sectionBreak(self, element, element_name, ancestors):
         debug("sb", "sectionBreak:", element)
 
     def parse_structuralElement(self, element, element_name, ancestors):
-        part_names = ('sectionBreak', 'tableOfContents', 'table', 'paragraph')
-        return self.parse(element, element_name='structuralElement', item_names=part_names, ancestors=ancestors)
+        part_names = ("sectionBreak", "tableOfContents", "table", "paragraph")
+        return self.parse(
+            element,
+            element_name="structuralElement",
+            item_names=part_names,
+            ancestors=ancestors,
+        )
 
     def parse_textRun(self, element, element_name, ancestors):
-        style = element.get('textStyle', "")
-        self.content.append((style, element['content']))
+        style = element.get("textStyle", "")
+        self.content.append((style, element["content"]))
         debug("tr", "tStyle:", style if style else "UNSTYLED CONTENT")
-        debug("tr", element['content'], end="|")
+        debug("tr", "<|", element["content"], end="|>")
+
+    def parse_textStyle(self, element, element_name, ancestors):
+        self.t_styles.push(element)
+        debug("ts", f"Pushed {element_name} {element!r}")
+        debug("ts", "tStyle now:")
+        for key, value in sorted(self.p_styles.to_dict().items()):
+            if value:
+                debug("ts", f"...{key!r}: {value!r}")
+        assert self.t_styles.pop() == element
 
     def parse_title(self, element, element_name, ancestors):
         self.title = element
-        debug("t", ".".join(ancestors+[element_name]), "is", element)
+        debug("t", ".".join(ancestors + [element_name]), "is", element)
 
     def parse_unrecognised_part_name(self, element, element_name, ancestors):
-        debug("upn", "Didn't handle", '.'.join(ancestors+[element_name]))
+        debug("upn", "Didn't handle", ".".join(ancestors + [element_name]))
 
 
-def main():
-    """Shows basic usage of the Docs API.
-    Begins attempt to create parser allowing transformation
-    into chosen publishing format.
+def main(document_id: str):
     """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
 
-    service = build('docs', 'v1', credentials=creds)
-
-    # Retrieve the documents contents from the Docs service.
-    document = service.documents().get(documentId=DOCUMENT_ID).execute()
-    with open(f"{DOCUMENT_ID}.json", "w") as fp:
-        json.dump(document, fp)
+    """
+    df = DocsFile(document_id).open()
+    document = json.loads(df.read())
+    document = ObjectDict(document)
     parser = MyDoc()
     parser.parse_document(element=document, ancestors=[])
 
-    debug("main", f'The title of document {DOCUMENT_ID} is: {document.get("title")!r}')
-    debug("main", f'  The parser says:', parser.title)
-    debug("main", f'                  ', parser.documentId)
+    debug("main", f'The title of the document is: {document.get("title")!r}')
+    debug("main", f"  The parser says: {parser.title}")
+    debug("main", f"                   {parser.documentId}")
 
-    from itertools import groupby
     current_style = None
     ci = iter(parser.content)
     for style, para in ci:
         if style != current_style:
             current_style = style
-            debug(f"\n:::::::: Style: {style} ::::::::")
-        debug(para, sep="", end='')
+            debug("final", f"\n:::::::: Style: {style} ::::::::")
+        debug("final", para, sep="", end="")
 
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main(sys.argv[1])
