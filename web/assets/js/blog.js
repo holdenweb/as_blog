00ca226639b3935226e1103031db5ea505c72bf6
Steve Holden <steve@holdenweb.com>
2020-11-23 12:02:44 +0000

    Updates to web assets.

--- web/assets/js/blog.js
+++ web/assets/js/blog.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	
-	/* ======= Highlight.js Plugin ======= */ 
-    /* Ref: https://highlightjs.org/usage/ */     
+
+	/* ======= Highlight.js Plugin ======= */
+    /* Ref: https://highlightjs.org/usage/ */
     $('pre code').each(function(i, block) {
 	    hljs.highlightBlock(block);
 	 });
