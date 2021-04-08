70e9a93234277d44dbba7d88b4162243c13b8308
Steve Holden <steve@holdenweb.com>
2021-04-08 21:07:22 +0100

    Refactor tests to remove monkeypatching and uses constants for readabiity.

--- tests/test_sep_concerns_7.py
+++ tests/test_sep_concerns_7.py
@@ -19,31 +19,28 @@ this_module = sys.modules[__name__]
     [(Storage, create_test_store), (SQL_Storage, SQL_create)],
     ids=["DBM", "SQL"],
 )
-def test_bills_for_range_by_user(monkeypatch, storage):
+def test_bills_for_range_by_user(storage):
     """
     Verify that bills can be extracted by user and date.
 
     """
     test_store, create_function = ts, cf = storage
-    monkeypatch.setattr(sep_concerns7, "Storage", test_store)
-    monkeypatch.setattr(sep_concerns7, "create_test_store", create_function)
-    create_test_store()
-    store = Storage("test")
+    create_function()
+    store = test_store("test")
     #
     # Current issue: print_and_save_bill2 creates its own
     # storage object and is not affected by the monkey patching.
     # We should inject the storage object via the API.
     #
-    print_and_save_bill2(
-        example_items, user="steve", store=store, date=datetime(2020, 1, 1)
-    )
-    b = dict(store.bills_for_range_by_user(datetime(2020, 1, 1), 1))
+    TEST_DATE_1 = datetime(2020, 1, 1)
+    print_and_save_bill2(example_items, user="steve", store=store, date=TEST_DATE_1)
+    b = dict(store.bills_for_range_by_user(TEST_DATE_1, 1))
     assert list(b.keys()) == ["steve"]
     bills = b["steve"]
     assert len(bills) == 1
     bill = bills[0]
     assert len(bill) == 2
-    assert b == dict(store.bills_for_range_by_user(datetime(2020, 1, 1), 25))
+    assert b == dict(store.bills_for_range_by_user(TEST_DATE_1, 25))
     # Verify that adding another bill for the same user does not affect existing results.
     print_and_save_bill2(
         example_items, user="steve", store=store, date=datetime(2020, 1, 2)
@@ -53,4 +50,4 @@ def test_bills_for_range_by_user(monkeypatch, storage):
     assert len(bills) == 1
     bill = bills[0]
     assert len(bill) == 2
-    bills = dict(store.bills_for_range_by_user(datetime(2020, 1, 1), 1))
+    bills = dict(store.bills_for_range_by_user(TEST_DATE_1, 1))
